d6c27b18e36d8bcc3ceb81e2c0b2663cfedfe37e
kellyalvesp <zkellypatriciaalves14@gmail.com>
2026-06-08 14:41:08 -0300

    fix: merge feat-7 into dev

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.e8rc63aprss"
+    "revision": "0.b17t7sse53g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
