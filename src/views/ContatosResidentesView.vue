ba55688e943c549ca079c3dc309c71df94f753f2
anapauladominoni <anapauladom29@gmail.com>
2026-08-04 15:12:06 -0300

    feat: Início da página contatos #12

---
+++ src/views/ContatosResidentesView.vue
@@ -0,0 +1,18 @@
+<template>
+<section>
+    <div class="telefone">
+        <img src="/src/assets/images/icone-telefone.png" alt="Ícone de telefone">
+        <span>Contatos</span>
+    </div>
+    <div>
+        <h1>RESPONSÁVEL 1:</h1>
+        <input type="text">
+    </div>
+</section>
+</template>
+
+<script setup>
+</script>
+<style scoped>
+
+</style>
