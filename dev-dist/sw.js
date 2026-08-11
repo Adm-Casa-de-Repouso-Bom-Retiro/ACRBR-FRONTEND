00f3a6779d60aca61b1c5b0ec2aae87f5bdda162
anapauladominoni <anapauladom29@gmail.com>
2026-08-11 14:34:05 -0300

    Merge FEAT-12 para Main

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ohn4gddmtco"
+    "revision": "0.0lqssbmr11g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
